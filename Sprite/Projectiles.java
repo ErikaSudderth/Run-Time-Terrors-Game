455fe4be786e9f39614c6b533375b51d283eee56
ErikaSudderth <erikasudderth@gmail.com>
2020-03-11 17:16:42 -0400

    Fixed constructors

--- Sprite/Projectiles.java
+++ Sprite/Projectiles.java
@@ -1,5 +1,7 @@
 package Sprite;
 
+import javafx.scene.image.ImageView;
+
 /**
  * Projectile class that extends Sprite. Each object will have a Boolean value
  * to determine if it has been thrown or not.
@@ -22,7 +24,7 @@ public class Projectiles extends Sprite {
      * @param _initialSpriteHeight
      * @param _spriteImage
      */
-    public Projectiles(int _initialSpriteXValue, int _initialSpriteYValue, int _initialSpriteWidth, int _initialSpriteHeight, String _spriteImage) {
+    public Projectiles(int _initialSpriteXValue, int _initialSpriteYValue, int _initialSpriteWidth, int _initialSpriteHeight, ImageView _spriteImage) {
 
         super (_initialSpriteXValue, _initialSpriteYValue, _initialSpriteWidth, _initialSpriteHeight, _spriteImage);
     }
