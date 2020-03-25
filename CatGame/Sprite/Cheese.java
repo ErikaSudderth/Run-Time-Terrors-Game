aa1dc31fa1a0e01b7e0c61c4d08b5b789233afdb
hasler zuniga <haslzun08@hotmail.com>
2020-03-25 13:55:38 -0400

    saving files

--- CatGame/Sprite/Cheese.java
+++ CatGame/Sprite/Cheese.java
@@ -3,22 +3,23 @@ package CatGame.Sprite;
 /**
  * Cheese class that extends the Sprite class. Each object will have a Boolean variable to determine if it has been collected by the mouse.
  *
- * @author Erika Sudderth Last updated: 3/19/20
+ * @author Erika Sudderth
+ * Last updated: 3/24/20
  */
 public class Cheese extends Sprite {
 
-	protected Boolean isCollected;
+    protected Boolean isCollected;
 
-	public Cheese() {
-	}
+    public Cheese() {
+    }
 
 //=================  GETTERS ===============
-	public Boolean getIsCollected() {
-		return this.isCollected;
-	}
+    public Boolean getIsCollected() {
+	return this.isCollected;
+    }
 
 //=================  SETTERS ===============
-	public void setIsCollected(Boolean _isCollected) {
-		this.isCollected = _isCollected;
-	}
+    public void setIsCollected(Boolean _isCollected) {
+	this.isCollected = _isCollected;
+    }
 }
